b50753ab69433a6db26150265c142e733c25035a
Desserat <igor19102000@gmail.com>
2026-05-08 17:31:00 +0300

    implemented orval, generated API methods & types, Bearer key moved to env.d.ts

--- src/shared/api/tmdbClient.ts
+++ src/shared/api/tmdbClient.ts
@@ -1,15 +1,21 @@
 import axios from 'axios'
+import type { AxiosRequestConfig } from 'axios'
 
-const TMDB_TOKEN =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMmY5YTRiMGFiZDY0YzU5MmY3MjNmZjU5ZTc4NTk5NCIsIm5iZiI6MTc3Nzk4OTMwOC4wNzgsInN1YiI6IjY5ZjlmNmJjZmUyNGI3YWZiZTU1ZjRjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rpdNvg0PfYmJpMcSavpBz7mmWJgSPsOP78Z1bzNe_Og'
-
-export const tmdbClient = axios.create({
+export const AXIOS_INSTANCE = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${TMDB_TOKEN}`,
-  },
-  params: {
-    language: 'ru-RU',
+    Authorization: `Bearer ${import.meta.VITE_TMDB_API_KEY}`,
   },
 })
+
+export const tmdbClient = <T>(config: AxiosRequestConfig): Promise<T> => {
+  const source = axios.CancelToken.source()
+  const promise = AXIOS_INSTANCE({ ...config, cancelToken: source.token }).then(({ data }) => data)
+
+  promise.cancel = () => {
+    source.cancel('Query was cancelled by Orval')
+  }
+
+  return promise
+}
